f8c283bf9d59dacb6a17526082628fc04bf508cd
Kshitij A <kshitij.everest@gmail.com>
2021-10-21 21:50:46 +0530

    Clean up

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-import matplotlib.pyplot as plt
-import scipy.stats as stats
-import xlsxwriter as xl
 
 path = "DataSet//dataSetFormat.xlsx"
 dataSet = pd.read_excel(path)
@@ -59,16 +55,4 @@ def marksToGrades(headCol, dataSet):
 res = marksToGrades(headerAndCol, dataSet)
 newDataSet = pd.DataFrame(res)
 newDataSet.to_excel("Grades.xlsx")
-# workbook = xl.Workbook('Grades.xlsx')
-# worksheet = workbook.add_worksheet()
-# row = 0
-# column = 0
 
-
-# mean = np.mean(col)
-# standardDeviation = np.std(col)
-# print(mean, standardDeviation)
-# dist = np.random.normal(mean, standardDeviation, size=len(col))
-# count, bins, ignored = plt.hist(dist, 30, density=True)
-# plt.plot(bins, 1/(standardDeviation * np.sqrt(2 * np.pi)) * np.exp( - (bins - mean)**2 / (2 * standardDeviation**2) ),linewidth=2, color='r')
-# plt.show()
