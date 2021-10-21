c75d4a82461cfa9846a5c0aa2c85de66fe81adac
Kshitij A <kshitij.everest@gmail.com>
2021-10-21 21:02:46 +0530

    Plotting done for all subjects normal distribution

--- main.py
+++ main.py
@@ -13,16 +13,18 @@ headerAndCol = {}
 for i in range(3, max_column):
     headerAndCol[dataSet.columns[i]] = dataSet.loc[:,dataSet.columns[i]].values
 
-# print(col)
-# figure, axis = plt.subplots(max_column/3, 3)
+figure, axis = plt.subplots(int((max_column-3)/3)+1,3)
+idx = 0
 for i in headerAndCol:
     mean = np.mean(headerAndCol[i])
     std = np.std(headerAndCol[i])
     dist = np.random.normal(mean, std, size=len(headerAndCol[i]))
-    count, bins, ignored = plt.hist(dist, 30, density=True)
-    plt.plot(bins, 1/(std * np.sqrt(2 * np.pi)) * np.exp( - (bins - mean)**2 / (2 * std**2) ),linewidth=2, color='r')
-    plt.title(i)
-    plt.show()
+    count, bins, ignored = axis[int(idx/3),idx%3].hist(dist, 30, density=True)
+    axis[int(idx/3),idx%3].plot(bins, 1/(std * np.sqrt(2 * np.pi)) * np.exp( - (bins - mean)**2 / (2 * std**2) ),linewidth=2, color='r')
+    axis[int(idx/3),idx%3].set_title(i)
+    idx += 1 
+
+plt.show()
 
 # mean = np.mean(col)
 # standardDeviation = np.std(col)
